8a9f8f7b771373ea7b7264422e888e890f39f8c9
AX028 <ax02846@gmail.com>
2025-03-10 16:18:21 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/submit', methods = ['POST'])
+def submit():
+  name = request.form.get('name')
+  email = request.form.get('email')
+  phone = request.form.get('phone')
+  message = request.form.get('message')
+  return render_template('submit.html', name = name, email = email, phone = phone, message = message)
+
